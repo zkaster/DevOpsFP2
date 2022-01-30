907f0d96758cbd4368d75c5fd92a3ff38f12bccd
Eugen <zkaster@gmail.com>
2022-01-30 18:47:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations! CHANGED 1222222</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <h1>EPAM DevOps courses. Yevhen Ryndych (aka zkaster)</h1>
+        <p>This is project with Proxmox + Jenkins + AWS Elastic Beanstalk <em>PHP</em> application is now running on own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
     </section>
